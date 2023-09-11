6a1898159a564615c7324a207f96dff42bd53c77
mrsujitsah <draj1750@gmail.com>
2023-09-11 11:44:12 +0530

    Project initialization

---
+++ src/main/java/com/mrsujitsah/bookmicroservice/app/BookMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.mrsujitsah.bookmicroservice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrsujitsah/bookmicroservice/app/BookMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrsujitsah.bookmicroservice.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
